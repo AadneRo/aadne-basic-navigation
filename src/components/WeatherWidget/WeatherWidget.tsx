7690f6f8afd7cceda166d9b981aa7be457f138bc
Ådne Rosenvinge <rosenvinge.adne@gmail.com>
2023-01-26 21:19:24 +0000

    Update add shell component WeatherWidget

--- src/components/WeatherWidget/WeatherWidget.tsx
+++ src/components/WeatherWidget/WeatherWidget.tsx
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 const WeatherWidget = () => {
-  
-  const [apiData, setApiData] = useState()
-  
-  useEffect(() => {
-      
-      
-      
-  });
+  const [apiData, setApiData] = useState();
+
+  useEffect(() => {});
 
   return <div></div>;
 };
