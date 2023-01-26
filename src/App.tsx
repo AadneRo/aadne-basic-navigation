a38909614beed223a078f80e64ee7b77410bd6f9
Ådne Rosenvinge <rosenvinge.adne@gmail.com>
2023-01-26 21:08:27 +0000

    Add location provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./styles.css";
 import { Map } from "./components";
+import LocationProvider from "./providers/LocationProvider";
 
 export default function App() {
   return (
     <div className="App">
-      <Map />
+      <LocationProvider>
+        <Map />
+      </LocationProvider>
     </div>
   );
 }
